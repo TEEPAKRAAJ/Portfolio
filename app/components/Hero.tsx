b1b182825f738d742922caa82d54c0597523d6d9
Teepakraaj <gteepak2003@gmail.com>
2025-07-14 21:49:24 +0530

    ver-8.3

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -25,7 +25,7 @@ export default function Hero() {
           <h1 className="text-cyan-200 text-5xl md:text-6xl lg:text-7xl font-extrabold font-serif mb-4">G Teepakraaj</h1>
 
           <h3 className="text-xl md:text-2xl lg:text-3xl text-white font-semibold flex justify-center lg:justify-start items-center gap-2 mb-4">
-            And I&apos;m a
+            And I'm a
             <span className="text-cyan-300 font-light">
               <ReactTyped
                 strings={[
@@ -56,7 +56,7 @@ export default function Hero() {
           {/* Buttons */}
           <div className="flex flex-col sm:flex-row gap-4 mt-8 justify-center lg:justify-start">
             <a href="#contact" className="px-6 py-3 bg-gradient-to-tr from-blue-500 via-cyan-300 to-blue-500 text-blue-900 hover:text-blue-950 rounded-full font-semibold hover:bg-gradient-to-tr hover:from-blue-600 hover:via-cyan-400 hover:to-blue-600 hover:scale-105 transition">Contact Me</a>
-            <a href="/showcase?tab=projects" className="px-6 py-3 border border-cyan-300 text-cyan-300 rounded-full font-semibold hover:bg-gradient-to-tr hover:from-blue-600 hover:via-cyan-400 hover:to-blue-600 hover:text-blue-950 hover:scale-105 transition">View Projects</a>
+            <a href="#showcase" className="px-6 py-3 border border-cyan-300 text-cyan-300 rounded-full font-semibold hover:bg-gradient-to-tr hover:from-blue-600 hover:via-cyan-400 hover:to-blue-600 hover:text-blue-950 hover:scale-105 transition">View Highlights</a>
           </div>
 
           {/* Social Icons */}
@@ -69,7 +69,7 @@ export default function Hero() {
 
         {/* Image Section */}
         <div className="flex-1 flex justify-center lg:justify-end relative">
-          <div className="relative w-[250px] md:w-[300px] lg:w-[350px] aspect-square group overflow-hidden">
+          <div className="relative w-[250px] md:w-[300px] lg:w-[350px] aspect-square group">
             {/* Glowing ring */}
             <div className="absolute inset-0 rounded-full bg-gradient-to-tr from-cyan-400 to-blue-400 blur-xl opacity-50 group-hover:opacity-80 transition duration-300"></div>
 
